6e87abffed6a3cccfac35f7ea8a0fadae3d3617d
Daniel <dasha19982009@rambler.ru>
2023-10-08 00:52:14 +0300

    bla

---
+++ str_funcs.py
@@ -0,0 +1,20 @@
+def uppercase_string(string):
+    print()
+    return string.upper()
+
+
+"""
+    Преобразует строку в верхний регистр.
+
+    Аргументы:
+    string (str): Исходная строка.
+
+    Возвращает:
+    str: Преобразованная строка в верхнем регистре.
+
+    Примеры использования:
+    >>> uppercase_string("hello")
+    'HELLO'
+    >>> uppercase_string("world")
+    'WORLD'...ffff
+"""
